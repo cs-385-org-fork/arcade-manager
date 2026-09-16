08daae5f56b956962c11dd20d093d633604d0006
Connor KLering <cklering02@gmail.com>
2026-09-16 14:47:43 -0400

    Set Up Play Card

--- arcade-manager/PlayCard.cs
+++ arcade-manager/PlayCard.cs
@@ -6,5 +6,20 @@ namespace arcade_manager
 {
     internal class PlayCard
     {
+        int cardID;
+        decimal moneyOnCard;
+        string customerName;
+        bool VIP;
+
+
+        private void topUpCard()
+        {
+
+        }
+
+        public void gamePaidFor()
+        {
+
+        } 
     }
 }
