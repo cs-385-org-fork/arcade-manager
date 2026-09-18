8cc7e1ab091ed4fbc428b7ddd51ec9085a243560
Trevor Past <trevorpastfb@gmail.com>
2026-09-18 19:32:27 -0400

    added machine class

---
+++ arcade-manager/Machine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arcade_manager
+{
+    internal class Machine
+    {
+        private string machineName = "";
+        private double machinePrice = 0;
+        private string machineStatus = "";
+        private bool onSale = false;
+
+        public string MachineName
+        {
+            get { return machineName; }
+            set { machineName = value; }
+        }
+        public double MachinePrice
+        {
+            get { return machinePrice; }
+            set { machinePrice = value; }
+        }
+        public string MachineStatus
+        {
+            get { return machineStatus; }
+            set { machineStatus = value; }
+        }
+        public bool OnSale
+        {
+            get { return onSale; }
+            set { onSale = value; }
+        }
+        public void ChangeStatus(string machineName)
+        {
+
+        }
+    }
+}
