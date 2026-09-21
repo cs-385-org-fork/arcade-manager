623a456f549675efecd8386f07fec0c21d0bc2e2
Miles Crane <263767342+mcrane-0@users.noreply.github.com>
2026-09-21 14:57:04 -0400

    Update Form1.cs

--- arcade-manager/Form1.cs
+++ arcade-manager/Form1.cs
@@ -1,5 +1,24 @@
 namespace arcade_manager {
     public partial class Form1 : Form {
+
+        Machine[] floorMachines = new Machine[6];
+
+        
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void saveCustomersToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void saveMachinesToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
+namespace arcade_manager {
+    public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
