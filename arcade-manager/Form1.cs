7b33adc1de52eb9f41a6bc64a86f5f999174368e
Connor KLering <cklering02@gmail.com>
2026-09-21 15:01:18 -0400

    Fix Form 1

--- arcade-manager/Form1.cs
+++ arcade-manager/Form1.cs
@@ -17,10 +17,3 @@ namespace arcade_manager {
         }
     }
 }
-namespace arcade_manager {
-    public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-        }
-    }
-}
